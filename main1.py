e3a4c9fc702a13d807c46f941b7f6bb327cc7ea1
charmin16 <charming_princewill@yahoo.com>
2024-08-21 05:14:25 +0100

    Second commit

--- main1.py
+++ main1.py
@@ -6,3 +6,5 @@ country_capital = {'Brazil': 'Brazilia',
 print(names)
 print(country_capital)
 print('come')
+print('our deepest fears is not that we are inadequate')
+print('Our deepest fear is that we are powerful beyond measures')
