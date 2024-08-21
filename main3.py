9b456cd5ee069ab0ceec20cfc2e4ac2dd16ffacf
charmin16 <charming_princewill@yahoo.com>
2024-08-21 22:04:23 +0100

    Third file added

---
+++ main3.py
@@ -0,0 +1,3 @@
+print('Our deepest fears is not that we are inadequate')
+print('Our deepest fear is that we are powerful beyond measures')
+print('And this is why I am running for President of the United States')
