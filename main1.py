64fbef78c7ddcbfcbc08634654c3341f2e80a094
charmin16 <charming_princewill@yahoo.com>
2024-08-21 04:50:34 +0100

    Initial setup

---
+++ main1.py
@@ -0,0 +1,8 @@
+names = ['Ibiba', 'Francis', 'Alaso']
+country_capital = {'Brazil': 'Brazilia',
+                   'Britain': 'London',
+                   'Sweden': 'Berlin'}
+
+print(names)
+print(country_capital)
+print('come')
