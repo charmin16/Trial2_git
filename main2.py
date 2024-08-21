bf007cbb07f21358d65b10d08f5c771c86966d23
charmin16 <charming_princewill@yahoo.com>
2024-08-21 05:24:39 +0100

    Added another python file called main_two

---
+++ main2.py
@@ -0,0 +1,8 @@
+staff = ['Ibiba', 'Francis', 'Alaso']
+country_caps = {'Brazil': 'Brazilia',
+                'Britain': 'London',
+                'Sweden': 'Berlin'}
+
+print(staff)
+print(country_caps)
+
